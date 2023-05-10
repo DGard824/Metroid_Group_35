af7d47c618927d1b62319c569c8517f0f9c6b40c
ajmegia27 <ajmegia@csuchico.edu>
2023-05-10 13:10:07 -0700

    Enemy

--- Metroid2/Assets/Scripts/RegularEnemy.cs
+++ Metroid2/Assets/Scripts/RegularEnemy.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class RegularEnemy : MonoBehaviour
 {
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+    public int speed;
+    public bool goingLeft;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftPos = leftPoint.transform.position;
+        rightPos = rightPoint.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (goingLeft == true)
+        {
+            if (transform.position.x <= leftPos.x)
+            {
+                goingLeft = false;
+            }
+            else
+            {
+                transform.position += Vector3.left * Time.deltaTime * speed;
+            }
+        }
+        else
+        {
+            if (transform.position.x >= rightPos.x)
+            {
+                goingLeft = true;
+            }
+            else
+            {
+                transform.position += Vector3.right * Time.deltaTime * speed;
+
+            }
+        }
     }
 }
+
+
